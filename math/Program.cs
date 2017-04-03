20f7d8a9c61e448ab4b8ce5fb4285c3d46bca02a
LIFE-BOOK <LIFE-BOOK@LIFE-BOOK-PC>
2017-04-03 13:25:41 +0300

    Add project files.

---
+++ math/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int var1 = 5;
+            //int var2 = 5;
+            //int temp1 = var1++;
+            //int temp2 = ++var2;
+            int MyVar = 7;
+            int MyVar1 = 2;
+
+            //Console.WriteLine("The Temp1 is :{0}\nThe Temp2 is :{1}", temp1, temp2);
+            int Temp = MyVar++ / MyVar1--;
+            Console.WriteLine("{0}", Temp);
+            Console.ReadKey();
+        }
+    }
+}
